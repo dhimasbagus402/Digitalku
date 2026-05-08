fb084bcf9f229f9980f13bd007f6f2120e35528a
dhimasbagus402 <dhimasbagus402@gmail.com>
2026-05-08 12:19:16 +0700

    Add files via upload

---
+++ fix_webview.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -rf ~/.wine
+
+sudo apt remove --purge wine* libwine* -y
+sudo apt autoremove -y
+
+sudo dpkg --add-architecture i386
+sudo apt update
+
+sudo apt install wine wine32 wine64 libwine fonts-wine wget -y
+
+timeout 10s winecfg || true
+
+wineserver -k
+
+winetricks -q corefonts vcrun2022 || true
+winetricks -q dotnet48 || true
+
+wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks -O winetricks
+
+chmod +x winetricks
+
+./winetricks -q webview2 || true
+
+echo "Script selesai dijalankan"
+EOF
